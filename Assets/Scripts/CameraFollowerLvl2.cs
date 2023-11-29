fa2f02c4e4123e3107d143b74536ad88b1815d23
GabrielaProszczuk <48457783+GabrielaProszczuk@users.noreply.github.com>
2023-11-29 11:13:57 +0100

    Update CameraFollowerLvl2.cs

--- Assets/Scripts/CameraFollowerLvl2.cs
+++ Assets/Scripts/CameraFollowerLvl2.cs
@@ -17,27 +17,6 @@ public class CameraFollowerLvl2 : MonoBehaviour
         Vector3 positionNow = transform.position;
         float desiredPositionY = target.position.y;
         float desiredPositionX = target.position.x;
-       /* Debug.Log("player: ");
-        Debug.Log(target.position);
-        Debug.Log("camera: ");
-        Debug.Log(transform.position);*/
-      /*  if (desiredPositionY >= -230)
-        {
-            desiredPositionY = -230;
-        }
-        if (desiredPositionY <= -700)
-        {
-            desiredPositionY = -700;
-        }
-
-        if (desiredPositionX >= 620)
-        {
-            desiredPositionX = 620;
-        }
-        if (desiredPositionX <= -280)
-        {
-            desiredPositionX = -280;
-        }*/
 
         Vector3 desiredPosition = new Vector3(desiredPositionX, desiredPositionY, positionNow.z);
         transform.position = Vector3.SmoothDamp(positionNow, desiredPosition, ref velocity, smoothSpeed);
