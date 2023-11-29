4702550714cf1da23f1446782b20d0e9a9a3857c
GabrielaProszczuk <48457783+GabrielaProszczuk@users.noreply.github.com>
2023-11-29 11:15:49 +0100

    Update MonsterNotFollowingController.cs

--- Assets/Scripts/MonsterNotFollowingController.cs
+++ Assets/Scripts/MonsterNotFollowingController.cs
@@ -8,13 +8,6 @@ public class MonsterNotFollowingController : MonoBehaviour
     float deltaX;
 
 
-  /*  public Text emotionText;
-    public GameObject emotionPlate;
-    public GameObject emotionSlider;
-    public Button exitEmotionPlateButton;
-
-    public GameObject monster;*/
-
 
     // Start is called before the first frame update
     void Start()
@@ -49,61 +42,7 @@ public class MonsterNotFollowingController : MonoBehaviour
             }
         }
 
-/*        prediction = PlayerPrefs.GetString("emotionLevel");
-        if (prediction != "")
-        {
-            value = Int32.Parse(prediction);
-            Debug.Log("val: " + value);
-            emotionSlider.GetComponent<Slider>().value = value;
-        }
-        if (PlayerPrefs.GetInt("monsterTouched") == 1)
-        {
-            emotionSlider.GetComponent<Slider>().value = value;
-            if (value >= 70)
-            {
-                emotionText.text = "Congratulations! Crush the monster!";
 
-            }
-            else if (value < 70 && prediction != "")
-            {
-                emotionText.text = "You lost the fight!" + "\n 'Cast' again!";
-                PlayerPrefs.SetInt("monsterDefeated", 0);
-                PlayerPrefs.SetInt("moveToStart", 1);
-                PlayerPrefs.SetString("emotionLevel", "");
-            }
-        }*/
     }
 
-   /* void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            PlayerPrefs.SetInt("monsterTouched", 1);
-
-            // Destroy(monster);
-
-            *//*get Color value from voice and python*//*
-
-
-            //emotionSlider.GetComponent<Slider>().value = value;
-
-
-            if (value >= 70)
-            {
-                Debug.Log("set value: " + value);
-                Destroy(monster);
-                emotionPlate.SetActive(false);
-                PlayerPrefs.SetInt("exitEmotionPlate", 1);
-                PlayerPrefs.SetInt("monsterDefeated", 1);
-                PlayerPrefs.SetString("emotionLevel", "");
-                emotionSlider.GetComponent<Slider>().value = value;
-                PlayerPrefs.SetInt("monsterTouched", 0);
-                // PlayerPrefs.SetInt("detect", 0);
-            }
-            else if (value < 70 && prediction != "")
-            {
-                PlayerPrefs.SetInt("monsterTouched", 1);
-            }
-        }
-    }*/
 }
